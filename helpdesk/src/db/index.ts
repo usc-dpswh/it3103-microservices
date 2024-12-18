1f1d28c8b1ff5270c3c02c2ff1e561bae14bed21
usc-dpswh <22103514@usc.edu.ph>
2024-12-18 19:42:05 +0800

    chore: recopy helpdesk/ directory

--- helpdesk/src/db/index.ts
+++
@@ -1,4 +0,0 @@
-import { drizzle } from "drizzle-orm/better-sqlite3"
-import * as schema from "./schema.js"
-
-export const db = drizzle("sqlite.db", { schema })
--- helpdesk/src/types/ticket.ts
+++
@@ -1,7 +0,0 @@
-import { createSelectSchema } from "drizzle-zod"
-import { z } from "zod"
-import { tickets } from "../db/schema.js"
-
-export const ticketSchema = createSelectSchema(tickets)
-
-export type Ticket = z.infer<typeof ticketSchema>
